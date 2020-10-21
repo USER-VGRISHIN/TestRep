1086d0bffa547b496ece16d40ff48dab6ae4fdcc
USER-VGRISHIN <vovaagrishinn@yandex.ru>
2020-10-22 02:47:23 +0300

    NEW

---
+++ Triangle.py
@@ -0,0 +1,23 @@
+import math
+class Triangle(object):
+    def __init__(self, x1, y1, x2, y2, x3, y3):
+        self.A = [x1, y1]
+        self.B = [x2, y2]
+        self.C = [x3, y3]
+        self.AB = math.sqrt(pow(self.B[0] - self.A[0], 2) + pow(self.B[1] - self.A[1], 2))
+        self.BC = math.sqrt(pow(self.C[0] - self.B[0], 2) + pow(self.C[1] - self.B[1], 2))
+        self.AC = math.sqrt(pow(self.C[0] - self.A[0], 2) + pow(self.C[1] - self.A[1], 2))
+
+    def triangleExist(self):
+
+        if (self.A[0] == self.B[0] or self.A[0] == self.C[0] or self.B[0] == self.C[0]) and \
+                (self.A[1] == self.B[1] or self.A[1] == self.C[1] or self.B[1] == self.C[1]):
+            return False
+        else:
+            return True
+
+    def getArea(self):
+        return 0.5 * abs((self.B[0] - self.A[0]) * (self.C[1] - self.A[1]) - (self.C[0] - self.A[0]) * (self.B[1] - self.A[1]))
+
+    def getPer(self):
+        return self.AB + self.AC + self.BC
---
+++ test_triangleexist.py
@@ -0,0 +1,26 @@
+from Triangle import  Triangle
+
+def test_tiangle_exist_positive_points():
+    t = Triangle(1, 2, 2, 7, 4, 16)
+    result = t.triangleExist()
+    assert result == True
+
+def test_tiangle_exist_negative_points():
+    t = Triangle(-1, -2, -2, -7, -4, -16)
+    result = t.triangleExist()
+    assert result == True
+
+def test_tiangle_degenerate():
+    t = Triangle(0, 1, 0, 7, 0, 3)
+    result = t.triangleExist()
+    assert result == True
+
+def test_tiangle_one_point():
+    t = Triangle(0, 0, 0, 0, 0, 0)
+    result = t.triangleExist()
+    assert result == False
+
+def test_tiangle_two_point_one_point():
+    t = Triangle(0, 2, 0, 2, 0, 7)
+    result = t.triangleExist()
+    assert result == False
